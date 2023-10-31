47069ada2e7ca5e8e658583988e17d60709b0c04
Motschik <tsukumotschik@gmail.com>
2023-10-31 20:48:09 +0900

    first commit.

---
+++ ui/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 360);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(360, 360);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ui/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace ui
+{
+    public partial class Form1 : Form
+    {
+        public const int SIZE = 360;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TopMost = true;
+            Size = new System.Drawing.Size(SIZE, SIZE);
+            pictureBox1.Size = new System.Drawing.Size(SIZE, SIZE);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool enableDance = true;
+
+        private void Dance()
+        {
+            while (enableDance)
+            {
+                VideoCapture vcap = new VideoCapture("ui.mp4");
+
+                while (vcap.IsOpened() && enableDance)
+                {
+                    Mat mat = new Mat();
+
+
+                    if (vcap.Read(mat))
+                    {
+                        if (pictureBox1.Image != null)
+                        {
+                            pictureBox1.Image.Dispose();
+                        }
+
+                        if (mat.IsContinuous())
+                        {
+                            Mat matRsz = mat.Clone(new Rect(280, 0, 720, 720));
+                            Mat hsvmat = matRsz.CvtColor(ColorConversionCodes.BGR2HSV);
+
+                            Mat rangeMat = hsvmat.InRange(new Scalar(50, 100, 100), new Scalar(72, 255, 255));
+
+                            Cv2.BitwiseNot(rangeMat, rangeMat);
+
+                            Mat dst = new Mat();
+                            matRsz.CopyTo(dst, rangeMat);
+                            Cv2.Resize(dst, dst, new OpenCvSharp.Size(SIZE, SIZE));
+
+                            Bitmap bmp = BitmapConverter.ToBitmap(dst);
+                            pictureBox1.Image = bmp;
+
+                            hsvmat.Dispose();
+                            rangeMat.Dispose();
+                            dst.Dispose();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        Application.DoEvents();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    Thread.Sleep((int)(300 / vcap.Fps));
+                    mat.Dispose();
+                }
+
+                vcap.Dispose();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                this.Invoke(new Action(() => Dance()));
+            });
+
+        }
+
+        private System.Drawing.Point p1;
+        private System.Drawing.Point p2;
+        private bool moving = false;
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1 = e.Location;
+            moving = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving)
+            {
+                p2 = System.Drawing.Point.Subtract(e.Location, (System.Drawing.Size)p1);
+                Location = System.Drawing.Point.Add(p2,(System.Drawing.Size)Location);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            enableDance = false;
+        }
+    }
+}
